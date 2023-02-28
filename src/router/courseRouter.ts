14b624dfdd1d3212200ab4e0a5a37493ecd1313d
Endi Oliveira <100172961+endioliveira@users.noreply.github.com>
2023-02-28 12:48:08 -0300

    exercicios de arquitetura ii

---
+++ src/router/courseRouter.ts
@@ -0,0 +1,8 @@
+import express from 'express'
+import { CourseController } from '../controller/CourseController'
+
+export const courseRouter = express.Router() 
+
+const courseController = new CourseController()
+
+courseRouter.get("/", courseController.getCourses)
